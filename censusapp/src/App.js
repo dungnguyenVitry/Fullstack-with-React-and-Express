43b82b910f5dc120e0b544140cfae7a821373ce7
dungnguyenVitry <dunglannion@protonmail.com>
2023-09-13 17:34:20 +0200

    ok

--- censusapp/src/App.js
+++ censusapp/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Census from './component/Census';
+
+function handleClick() {
+  window.location.reload();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <Census></Census>
+      </p>
+      <button onClick={handleClick}>Random other city</button>
     </div>
+
   );
 }
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb://localhost:27017/census';
+const mongoDB = 'mongodb://localhost:27017/censusdb';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
@@ -15,7 +15,13 @@ db.on('connected', console.log.bind(console, 'Mongo connect sucessfully'))
 const express = require('express');
 const app = express()
 
-const port = 8082;
+const port = 8081;
+app.use(function (req, res, next){
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 app.use('/api', require('./route/request'));
 
