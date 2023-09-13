50c7e20040900d4d62510c0489acb06663354a46
dungnguyenVitry <dunglannion@protonmail.com>
2023-09-13 16:35:24 +0200

    set data on mongodb and express

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+// declare mongoDB
 
-   
+const mongoose = require('mongoose');
 
+const mongoDB = 'mongodb://localhost:27017/census';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+
+// check connection 
+db.on('error', console.error.bind(console, 'failed to connect Mongo'));
+db.on('connected', console.log.bind(console, 'Mongo connect sucessfully'))
+
+//Express
+const express = require('express');
+const app = express()
+
+const port = 8082;
+app.use(express.json());
+app.use('/api', require('./route/request'));
+
+app.listen(port, () => {
+    console.log(`server listening on ${port}`)
+})
